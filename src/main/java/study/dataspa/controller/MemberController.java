372bb2f49c9d78b058da2a4a99acb1b67e862502
mygoaway <mygoaway2@gmail.com>
2022-04-02 13:20:25 +0900

    part6 도메인 클래스 컨버터 구현

---
+++ src/main/java/study/dataspa/controller/MemberController.java
@@ -0,0 +1,33 @@
+package study.dataspa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.dataspa.entity.Member;
+import study.dataspa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members1/{id}")
+    public String findMember1(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        Member member = new Member("member1");
+        memberRepository.save(member);
+    }
+}
